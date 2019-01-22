5d0d584676f72c952de9dc65b234728a0be8e58a
Schiele <thomasschiele95@googlemail.com>
2019-01-22 16:22:33 +0100

    Neues Nav

--- js/click.js
+++ js/click.js
@@ -1,11 +1,29 @@
 function toggleNavbar() {
-    if($('.nav-icon').hasClass("toggle")) {
-        $('.nav-icon').removeClass("toggle");
-        $('.nav').removeClass("toggle");
-        $('.container').removeClass("toggle");
+    if ($('.nav-icon').hasClass("active")) {
+        $('.nav-icon').removeClass("active");
+        $('.nav').removeClass("active");
+        $('.container').removeClass("active");
     } else {
-        $('.nav-icon').addClass("toggle");
-        $('.nav').addClass("toggle");
-        $('.container').addClass("toggle");
+        $('.nav-icon').addClass("active");
+        $('.nav').addClass("active");
+        $('.container').addClass("active");
     }
 }
+
+
+$(document).ready(function() {
+    $('.nav-list-item').on('click', function() {
+        if (!$(this).hasClass("active")) {
+            $('.nav-list-item').removeClass("active");
+            $(this).addClass("active");
+            var index = $(this).attr("data-index");
+            var color = $(this).attr("data-color");
+            $('.nav-list-indicator').css("marginLeft", (25 * index) + "%");
+            $('.nav-list-indicator').css("background", color);
+        }
+        setTimeout(function() {
+            $('.nav').removeClass("active");
+            toggleNavbar();
+        }, 400);
+    });
+});
